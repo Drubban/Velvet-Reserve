6c3c1bb9af02c7ca11aa96cf793f5de44b8bf7b5
Yair Romero <155039644+Drubban@users.noreply.github.com>
2025-08-02 12:46:52 -0600

    feat(master):
    add database

---
+++ database/migrations/2025_08_02_163958_create_telefono_table.php.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('telefono', function (Blueprint $table) {
+            $table->id('id_telefono');
+            $table->unsignedBigInteger('id_cliente');
+            $table->string('lada', 5)->nullable();
+            $table->unsignedBigInteger('numero_telefono');
+            $table->string('tipo', 20); // Solo una vez
+            $table->timestamps();
+
+            $table->foreign('id_cliente')->references('id_cliente')->on('clientes')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('telefono');
+    }
+};
